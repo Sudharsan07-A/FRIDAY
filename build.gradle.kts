57ee5317976ee3ff79fd24250e00888a0724ba54
Sudharsan07-A <110477070+Sudharsan07-A@users.noreply.github.com>
2026-09-15 12:32:48 +0530

    Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Root build.gradle.kts
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    kotlin("android") version "1.9.10" apply false
+    kotlin("jvm") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
+}
